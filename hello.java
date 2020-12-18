d685f4e8099ae3f2e6701bda5066aa52e62e6d9a
shohel <asaduzzamann84@gmail.com>
2020-12-17 23:06:41 -0800

    Added a new method called greeting to hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Vs code is awesome...");
+
+        gretting("Asaduzzaman Shohel");
+    }
+
+    public static void gretting(String name){
+        System.out.println("Welcome " + name);
+    }
+}
