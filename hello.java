4038dbd0863a7da9979852b7bc9d6975708ea536
shohel <asaduzzamann84@gmail.com>
2020-12-18 00:08:41 -0800

    Change greeting method

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Vs code is awesome...");
 
-        gretting("Asaduzzaman Shohel");
+        gretting("Git");
     }
 
     public static void gretting(String name){
-        System.out.println("Welcome " + name);
+        System.out.println("Bye Bye " + name);
     }
 }
