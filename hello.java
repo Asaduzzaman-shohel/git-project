dfb6a08b38b50ecda067fba47ee59fc131addf7c
shohel <asaduzzamann84@gmail.com>
2020-12-18 15:50:06 -0800

    Add message method merge from anotherBranch

--- hello.java
+++ hello.java
@@ -9,18 +9,16 @@ public class Hello {
     }
 
 
-<<<<<<< HEAD
     public static void display(){
         System.out.println("Hello Hello");
-
     }
 
     
     public static int sqr(int x){
         return x*x;
-=======
+    }    
+
     public static void meggage(){
         System.out.println("Hooray!!! Now we are able to contribute on open source...");
->>>>>>> anotherBranch
     }
 }
